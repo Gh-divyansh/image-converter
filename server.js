f834ef9bb25aff13ba75e072b751ed9635c52f6f
Gh-divyansh <divyansh.tailort20@gmail.com>
2026-05-28 12:32:37 +0530

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const convertHandler = require("./api/convert");
 
@@ -7,14 +8,20 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/convert", (req, res) => {
   return convertHandler(req, res);
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "public", "index.html")
+  );
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
